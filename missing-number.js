2987a824f365f27648c3461c1da138e3d03c0e3a
Manikanta Maddala <manikanta.maddala@ggktech.com>
2020-01-26 20:17:24 +0530

    Missing number

---
+++ missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    var sum = 0;
+    for(var i=0; i<nums.length; i++){
+        sum += nums[i]; 
+    }
+    return (nums.length * (nums.length+1))/2 - sum;
+};
